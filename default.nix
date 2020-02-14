d0be262510d3c4fb2f9c4aa155c55bde73d9c8a6
Sean Hickman <sean@grumply.com>
2020-02-13 23:34:52 -0600

    Update for GHC/JS 8.6.5 and 2020

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, random, vector, primitive, stdenv }:
 mkDerivation {
   pname = "pure-random-pcg";
-  version = "0.7.0.0";
+  version = "0.8.0.0";
   src = ./.;
   libraryHaskellDepends = [ base random vector primitive ];
   homepage = "github.com/grumply/pure-random-pcg";
